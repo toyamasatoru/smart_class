c9d0c4fe21acc165f36c5fefd7602ff64570f4e2
to-yan <t.satoru03@gmail.com>
2021-09-02 18:06:09 +0900

    ページ修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   get 'posts/index'
   devise_for :users
   root to: 'posts#index'
+  resources :users, only: [:index, :edit, :update, :show, :destory]
   resources :posts, only: [:index, :new, :create, :destroy, :edit, :update] do
     resource :likes, only: [:create, :destroy]
-    collection do
-      get 'quiz'
-    end
   end
+  get '/quiz', to: 'quiz#show'
+  get '/quiz2', to: 'quiz2#show'
 end
